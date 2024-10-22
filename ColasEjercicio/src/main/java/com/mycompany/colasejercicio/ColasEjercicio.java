bb863e46f0f986386389bdbe2c67e654328fbda6
Santiago Guevara <saguevaraq@ufpso.edu.co>
2024-10-22 07:09:19 -0500

    Tarea Colas

---
+++ ColasEjercicio/src/main/java/com/mycompany/colasejercicio/ColasEjercicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.colasejercicio;
+
+/**
+ *
+ * @author SCIS2-27
+ */
+public class ColasEjercicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
